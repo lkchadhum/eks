c209b98c3091407f58964aa0c74fe983f1efcbcd
lava <lavakumarreddy974@gmail.com>
2020-12-06 21:52:19 -0600

    hot six

--- nodegroup.tf
+++ nodegroup.tf
@@ -1,18 +1,23 @@
-resource "aws_eks_node_group" "nodegroups" {  
-  cluster_name    = "${aws_eks_cluster.eks_cluster.name}"  
-  node_group_name = "${var.aws_eks_nodegroup_name}"
-  # check the node arn for this   
-  node_role_arn   = "${aws_iam_role.eks_nodegroup_role.arn}"  
-  subnet_ids      = "${var.aws_eks_node_subnets}"  
-  instance_types  = "${var.aws_eks_node_instance_size}"  
-  scaling_config {    
-    desired_size = "${var.aws_eks_node_desired_size}"    
-    max_size     = "${var.aws_eks_node_max_size}"    
-    min_size     = "${var.aws_eks_node_min_size}"  
-    }
-  depends_on = [  
-        aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,  
-        aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,  
-        aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,  
-       ]
-    }
+resource "aws_eks_node_group" "nodegroups" {
+  cluster_name    = aws_eks_cluster.eks_cluster.name
+  node_group_name = var.aws_eks_nodegroup_name
+
+  # check the node arn for this 
+  node_role_arn   = aws_iam_role.eks_nodegroup_role.arn
+  subnet_ids      = var.subnet_ids
+  instance_types  = var.aws_eks_node_instance_size
+  scaling_config {
+    desired_size = var.aws_eks_node_desired_size
+    max_size     = var.aws_eks_node_max_size
+    min_size     = var.aws_eks_node_min_size
+  }
+
+########## check the roles and policy attached here
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
+
+##### encryption
--- variables.tf
+++ variables.tf
@@ -1,20 +1,103 @@
+variable "namespaces" {
+  type        = list(string)
+  description = "List of namespaces to be created in our EKS Cluster."
+}
 
-resource "aws_eks_cluster" "eks" {
-  name = var.cluster-name
+# variable "eks_cluster_role_name" {
+#   type        = string
+#   description = "role name for eks cluster"
+# }
 
-  version = var.k8s-version
+variable "eks_cluster_nodegroup_role_name" {
+  type        = string
+  description = "role name for eks cluster"
+}
 
-  role_arn = aws_iam_role.cluster.arn
+variable "eks_cluster_name" {
+  type        = string
+  description = "eks cluster name"
+}
 
-  vpc_config {
-    security_group_ids = [data.aws_security_group.cluster.id]
-    subnet_ids         = data.aws_subnet_ids.private.ids
-  }
+# variable "cluster_enabled_log_types" {
+#   type        = string
+# }
 
-  enabled_cluster_log_types = var.eks-cw-logging
+variable "cluster_version" {
+  type        = any
+  description = "kubernetes version"
+}
 
-  depends_on = [
-    aws_iam_role_policy_attachment.cluster-AmazonEKSClusterPolicy,
-    aws_iam_role_policy_attachment.cluster-AmazonEKSServicePolicy,
-  ]
+variable "tags" {
+  type        = map(string)
+  default     = {}
+}
+
+# variable "subnets" {
+#   type        = list(string)
+#   description = "List of subnets to be created in our EKS Cluster."
+# }
+variable "aws_eks_node_instance_size" {
+  type        = list(string)
+}
+variable "region" {
+  type        = string
+  description = "AWS Region"
+}
+
+variable "aws_eks_node_desired_size" {
+  type        = any
+}
+
+variable "aws_eks_node_max_size" {
+  type        = any
+}
+
+variable "aws_eks_nodegroup_name" {
+  type        = string
+}
+
+variable "aws_eks_node_min_size" {
+  type        = any
+}
+
+# variable "eks_cluster_security_group" {
+#   type        = string
+#   description = "eks_cluster_security_group name"
+# }
+
+# variable "vpc_id" {
+#   type        = string
+#   description = "vpc id"
+# }
+
+variable "sg_tags" {
+  type        = map(string)
+  default     = {}
+}
+
+
+
+// networking
+
+# variable "private_subnets" {
+#   type        = string
+#   description = "List of private subnet CIDR blocks"
+# }
+# variable "vpc_cidr" {
+#   type        = string
+  
+# }
+
+# variable "azs" {
+#   type        = string
+#   default     = "us-east-1a"
+# }
+variable "subnet_ids" {
+  type        = list(string)
+}
+variable "eks_cluster_role_name" {
+  type        = string
+}
+variable "eks_nodegroup_role_name" {
+  type        = string
 }
